983616a98d0d49a675ee13bc9da5dc0e96f5e37d
Pepikamp <l.m.l.peperkamp@student.tue.nl>
2026-01-13 17:11:22 +0100

    streamlit run command uitleg aangepast

--- src/app.py/app.py
+++ src/app.py/app.py
@@ -9,7 +9,7 @@ HOW TO RUN THIS APP:
        pip install streamlit pandas matplotlib seaborn plotly numpy
 
 3. Run the Streamlit app (from the project root):
-       streamlit run src/app.py
+       streamlit run src/app.py/app.py
 
 4. The app will open automatically in your browser.
    If not, look for the URL printed in the terminal (usually http://localhost:8501).
